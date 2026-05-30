a6a385c9c3a8070cd7ed405a0be1e7a1c70e48a2
jonisb <github.com@JsBComputing.se>
2026-05-30 23:31:32 +0200

    Some minimal code

---
+++ src/loader/loader.cpp
@@ -0,0 +1,7 @@
+#include "../common_log.hpp"
+#include "loader.hpp"
+#include <sstream>
+
+extern "C" IMAGE_DOS_HEADER __ImageBase;
+
+
---
+++ src/loader/loader.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <windows.h>
+#include <string>
+#include <memory>
+#include "../../include/plugin.hpp"
